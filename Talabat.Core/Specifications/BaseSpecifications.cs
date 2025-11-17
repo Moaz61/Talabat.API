e03606655851e18b917e3aae1c752e2623a03c03
moaz <moaz43700@gmail.com>
2025-11-17 23:25:36 +0200

    1.Specification Design Pattern

---
+++ Talabat.Core/Specifications/BaseSpecifications.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Specifications
+{
+    public class BaseSpecifications<T> : ISpecifications<T> where T : BaseEntity
+    {
+        public Expression<Func<T, bool>>? Criteria { get; set; } = null;
+        public List<Expression<Func<T, object>>> Includes { get; set; } = new List<Expression<Func<T, object>>>();
+        //3mlt inizialize hena bdl m a3mlo kol mra f constructor 3shan hya zy m hya msh bghyr
+
+        public BaseSpecifications()
+        {
+            //Criteria = null
+        }
+        public BaseSpecifications(Expression<Func<T, bool>> criteriaExpression)
+        {
+            Criteria = criteriaExpression;
+        }
+    }
+}
