d3a822adfaf83548cc35f91022d002118f54586d
moaz <moaz43700@gmail.com>
2025-11-19 00:21:08 +0200

    2.Refactor GetProducts Using Specification

---
+++ Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Specifications.Product_Specs
+{
+    public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
+    {
+        //This Constructor will be used to create object that will be used to get All Products
+
+        public ProductWithBrandAndCategorySpecifications() : base()
+        {
+            Includes.Add(P => P.Brand);
+            Includes.Add(P => P.Category);
+        }
+    }
+}
