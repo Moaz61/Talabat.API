38c64748fdfe3b4070e064947938117e012d9d9f
moaz <moaz43700@gmail.com>
2025-11-19 00:36:24 +0200

    3.Refactor GetProductsById Using Specifications

--- Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
+++ Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
@@ -10,8 +10,20 @@ namespace Talabat.Core.Specifications.Product_Specs
     public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
     {
         //This Constructor will be used to create object that will be used to get All Products
+        public ProductWithBrandAndCategorySpecifications()
+            : base()
+        {
+            AddIncludes();
+        }
 
-        public ProductWithBrandAndCategorySpecifications() : base()
+        //This Constructor will be used to create object that will be used to get a specific Product With Id
+        public ProductWithBrandAndCategorySpecifications(int id)
+            : base(P => P.Id == id)
+        {
+            AddIncludes();
+        }
+
+        private void AddIncludes()
         {
             Includes.Add(P => P.Brand);
             Includes.Add(P => P.Category);
