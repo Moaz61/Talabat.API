72354d1f61db6db35018908b19a24406bcb8ee24
moaz <moaz43700@gmail.com>
2025-12-18 00:41:36 +0200

    2. part2 Identity Users Data Seeding

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
@@ -6,6 +7,7 @@ using Talabat.APIs.Extensions;
 using Talabat.APIs.Helpers;
 using Talabat.APIs.Middlewares;
 using Talabat.Core.Entities;
+using Talabat.Core.Entities.Identity;
 using Talabat.Core.Repositories.Contract;
 using Talabat.Infrastructure._Identity;
 using Talabat.Infrastructure.Basket_Repository;
@@ -46,6 +48,9 @@ namespace Talabat.APIs
                 options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("IdentityConnection"));
             });
 
+            webApplicationBuilder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>();
+
             #endregion
 
             var app = webApplicationBuilder.Build();
@@ -67,6 +72,9 @@ namespace Talabat.APIs
                 await StoreContextSeed.SeedAsync(_dbContext); // Data Seeding
 
                 await _identityDbContext.Database.MigrateAsync(); //Update Database
+
+                var _userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                await ApplicationIdentityContextSeed.SeedUserAsync(_userManager);
             }
             catch (Exception ex)
             {
