9e6f39f1de0d47f94c3e5269399edb886eca391f
moaz <moaz43700@gmail.com>
2025-12-18 02:18:52 +0200

    4. Register Endpoint

--- Talabat.APIs/Controllers/AccountController.cs
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -39,5 +39,29 @@ namespace Talabat.APIs.Controllers
                 Token = "This will be token"
             });
         }
+
+
+        [HttpPost("register")]  //POST : /api/Account/register
+        public async Task< ActionResult<UserDto>> Register(RegisterDto model)
+        {
+            var user = new ApplicationUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                UserName = model.Email.Split("@")[0],
+                PhoneNumber = model.Phone
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded) return BadRequest(new ApiValidationErrorResponse() { Errors = result.Errors.Select(E => E.Description) });
+
+            return Ok(new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "This will be token"
+            });
+        }
     }
 }
---
+++ Talabat.APIs/Dtos/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talabat.APIs.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string DisplayName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Phone { get; set; } = null!;
+
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+            ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 Number, 1 Non Alphanumeric and at least 6 Characters")]
+        public string Password { get; set; } = null!;
+    }
+}
