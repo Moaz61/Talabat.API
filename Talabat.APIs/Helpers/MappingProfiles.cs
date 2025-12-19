ddd568b47980bb175d3d581ef989e5993199231c
moaz <moaz43700@gmail.com>
2025-12-20 01:55:46 +0200

    10. Update User Address Endpoint

--- Talabat.APIs/Helpers/MappingProfiles.cs
+++ Talabat.APIs/Helpers/MappingProfiles.cs
@@ -19,7 +19,7 @@ namespace Talabat.APIs.Helpers
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
 
-            CreateMap<Address, AddressDto>();
+            CreateMap<Address, AddressDto>().ReverseMap();
         }
     }
 }
